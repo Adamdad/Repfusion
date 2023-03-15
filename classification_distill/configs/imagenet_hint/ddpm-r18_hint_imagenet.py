0e6709619f6e3e18b8ce63655f496d32735dd967
Adamdad <787707188@qq.com>
2023-03-16 01:53:50 +0800

    cifar10

--- classification_distill/configs/imagenet_hint/ddpm-r18_hint_imagenet.py
+++ classification_distill/configs/imagenet_hint/ddpm-r18_hint_imagenet.py
@@ -11,7 +11,7 @@ data = dict(
     test=dict(ann_file=None))
 
 # checkpoint saving
-checkpoint_config = dict(interval=50)
+checkpoint_config = dict(interval=10)
 # yapf:disable
 log_config = dict(
     interval=100,
@@ -27,34 +27,24 @@ load_from = None
 resume_from = None
 workflow = [('train', 1)]
 
-optimizer = dict(
-    type='AdamW',
-    lr=5e-4 * 32 * 8 / 512)
-
-# optimizer
-lr_config = dict(
-    policy='CosineAnnealing',
-    by_epoch=False,
-    min_lr_ratio=1e-2,
-    warmup='linear',
-    warmup_ratio=1e-3,
-    warmup_iters=5 * 1252,
-    warmup_by_epoch=False)
-
-runner = dict(type='EpochBasedRunner', max_epochs=100)
+optimizer = dict(type='SGD',
+                 lr=0.1, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(policy='step', step=[100, 150])
+runner = dict(type='EpochBasedRunner', max_epochs=200)
 
 fp16 = dict(loss_scale=512.)
 # model settings
 model = dict(
-    type='KDDDPM_Pretrain_Imagenet_TaskOriented_ImageClassifier',
+    type='KDDDPM_Pretrain_Imagenet256_ImageClassifier',
     teacher_layers=[["middle_block.2.out_layers.3", 1024]],
     student_layers=[['backbone.layer4.1.relu', 512]],
     distill_fn=[['l2', 1.0]],
     teacher_dim=1024,
     num_class=1000,
     teacher_ckp="/home/yangxingyi/guided-diffusion/256x256_diffusion_uncond.pt",
-    train_cfg=dict(kd_weight=1.,
-                entropy_reg=0.0),
+    train_cfg=dict(kd_weight=1.),
     backbone=dict(type='ResNet',
                      depth=18,
                      num_stages=4,
